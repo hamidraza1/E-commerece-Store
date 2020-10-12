84c47b41926d4deb69e38760285c91b063f5dfdb
hamidraza1 <hrazaa306@gmail.com>
2020-10-12 08:15:31 +0200

    issue resolved

--- src/components/TagContentfulDescription/index.js
+++ src/components/TagContentfulDescription/index.js
@@ -21,15 +21,26 @@ const query = graphql`
 export function TagContentfulDescription() {
   const { allContentfulBannerPost } = useStaticQuery(query);
   /*  const { products, collections } = React.useContext(ProductContext); */
-  const URLidHandle = window.location.pathname.replace('/products/', '');
-  const ContentPageBannerData = allContentfulBannerPost.edges.find(
-    ({ node }) => node.title === URLidHandle
-  );
+  const [URLidHandle, setURLidHandle] = React.useState();
+
+  React.useEffect(() => {
+    setURLidHandle(window.location.pathname.replace('/products/', ''));
+  }, []);
+
+  const ContentPageBannerData = URLidHandle
+    ? allContentfulBannerPost.edges.find(
+        ({ node }) => node.title === URLidHandle
+      )
+    : '';
 
   return (
     <TagDescriptionWrapper>
-      <h3>{ContentPageBannerData?.node.title}</h3>
-      <p>{ContentPageBannerData?.node.description.description}</p>
+      <h3>{ContentPageBannerData ? ContentPageBannerData.node.title : ''}</h3>
+      <p>
+        {ContentPageBannerData
+          ? ContentPageBannerData.node.description.description
+          : ''}
+      </p>
     </TagDescriptionWrapper>
   );
 }
