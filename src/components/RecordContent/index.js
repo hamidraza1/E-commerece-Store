a3613632e25987492bb2fe2d0c7669e9d34f9cb9
hamidraza1 <hrazaa306@gmail.com>
2020-10-17 23:49:08 +0200

    contact form completed

--- src/components/RecordContent/index.js
+++ src/components/RecordContent/index.js
@@ -47,84 +47,86 @@ export function RecordContent() {
 
   return (
     <RecordContentWrapper>
-      {Patients?.Patients.map((patient, i) => (
-        <RecordInnerContent key={i}>
-          <Header>
-            <div>
-              <FirstName>
-                <span>FirstName:</span>
-                <span>{patient.firstName}</span>
-              </FirstName>
-              <LastName>
-                <span>LastName:</span>
-                <span>{patient.lastName}</span>
-              </LastName>
-              <Gender>
-                <span>Gender:</span>
-                <span>{patient.gender}</span>
-              </Gender>
-            </div>
-            <div>
-              <Email>
-                <span>Email:</span>
-                <span>{patient.email}</span>
-              </Email>
-              <FirstContact>
-                <span>Contact:</span>
-                <span>{patient.firstContact}</span>
-              </FirstContact>
-              <DOB>
-                <span>DOB:</span>
-                <span>{patient.dob}</span>
-              </DOB>
-            </div>
-          </Header>
-          <OtherMan>
-            <SecondName>
-              <span>Who is the medicine for?</span>
-              <span>{patient.secondName}</span>
-            </SecondName>
-            <SecondDOB>
-              <span>Contact:</span>
-              <span>{patient.secondContact}</span>
-            </SecondDOB>
-            <SecondDOB>
-              <span>DOB:</span>
-              <span>{patient.seconddob}</span>
-            </SecondDOB>
-          </OtherMan>
-          <Details>
-            <Symptoms>
-              <span>Symptoms:</span>
-              <span>{patient.symptoms}</span>
-            </Symptoms>
-            <SymptomsDuration>
-              <span>Symptoms Duration:</span>
-              <span>{patient.symptomsduration}</span>
-            </SymptomsDuration>
-            <PreviousMedicaton>
-              <span>Previous Medication:</span>
-              <span>{patient.previousMedication}</span>
-            </PreviousMedicaton>
-            <OnGoingMedication>
-              <span>OnGoing Medication:</span>
-              <span>{patient.onGoingMedication}</span>
-            </OnGoingMedication>
-            <Pregnant>
-              <span>Pregnant?</span>
-              <span>{patient.pregnant}</span>
-            </Pregnant>
-            <Date>
-              <span>Date:</span>
-              <span>{patient.date}</span>
-            </Date>
-            <Time>
-              <span>Time:</span>
-              <span>{patient.time}</span>
-            </Time>
-          </Details>
-        </RecordInnerContent>
-      ))}
+      {Patients
+        ? Patients.Patients.map((patient, i) => (
+            <RecordInnerContent key={i}>
+              <Header>
+                <div>
+                  <FirstName>
+                    <span>FirstName:</span>
+                    <span>{patient.firstName}</span>
+                  </FirstName>
+                  <LastName>
+                    <span>LastName:</span>
+                    <span>{patient.lastName}</span>
+                  </LastName>
+                  <Gender>
+                    <span>Gender:</span>
+                    <span>{patient.gender}</span>
+                  </Gender>
+                </div>
+                <div>
+                  <Email>
+                    <span>Email:</span>
+                    <span>{patient.email}</span>
+                  </Email>
+                  <FirstContact>
+                    <span>Contact:</span>
+                    <span>{patient.firstContact}</span>
+                  </FirstContact>
+                  <DOB>
+                    <span>DOB:</span>
+                    <span>{patient.dob}</span>
+                  </DOB>
+                </div>
+              </Header>
+              <OtherMan>
+                <SecondName>
+                  <span>Who is the medicine for?</span>
+                  <span>{patient.secondName}</span>
+                </SecondName>
+                <SecondDOB>
+                  <span>Contact:</span>
+                  <span>{patient.secondContact}</span>
+                </SecondDOB>
+                <SecondDOB>
+                  <span>DOB:</span>
+                  <span>{patient.seconddob}</span>
+                </SecondDOB>
+              </OtherMan>
+              <Details>
+                <Symptoms>
+                  <span>Symptoms:</span>
+                  <span>{patient.symptoms}</span>
+                </Symptoms>
+                <SymptomsDuration>
+                  <span>Symptoms Duration:</span>
+                  <span>{patient.symptomsduration}</span>
+                </SymptomsDuration>
+                <PreviousMedicaton>
+                  <span>Previous Medication:</span>
+                  <span>{patient.previousMedication}</span>
+                </PreviousMedicaton>
+                <OnGoingMedication>
+                  <span>OnGoing Medication:</span>
+                  <span>{patient.onGoingMedication}</span>
+                </OnGoingMedication>
+                <Pregnant>
+                  <span>Pregnant?</span>
+                  <span>{patient.pregnant}</span>
+                </Pregnant>
+                <Date>
+                  <span>Date:</span>
+                  <span>{patient.date}</span>
+                </Date>
+                <Time>
+                  <span>Time:</span>
+                  <span>{patient.time}</span>
+                </Time>
+              </Details>
+            </RecordInnerContent>
+          ))
+        : ''}
     </RecordContentWrapper>
   );
 }
