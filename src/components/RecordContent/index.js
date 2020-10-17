95dae8377f9e9b21d4672055d294e92fa580b265
hamidraza1 <hrazaa306@gmail.com>
2020-10-18 00:05:05 +0200

    contact form completed

--- src/components/RecordContent/index.js
+++ src/components/RecordContent/index.js
@@ -29,104 +29,104 @@ import {
 export function RecordContent() {
   let [Patients, setPatient] = React.useState(null);
 
-  firebase
-    .firestore()
-    .collection('Contacts')
-    .orderBy('timestamp', 'desc')
-    .get()
-    .then(function (querySnapshot) {
-      const PatientsData = [];
-      querySnapshot.forEach(doc => {
-        const data = doc.data();
-        data.date = doc.data().timestamp.toDate().toDateString();
-        data.time = doc.data().timestamp.toDate().toLocaleTimeString();
-        PatientsData.push(data);
+  React.useEffect(() => {
+    firebase
+      .firestore()
+      .collection('Contacts')
+      .orderBy('timestamp', 'desc')
+      .get()
+      .then(function (querySnapshot) {
+        const PatientsData = [];
+        querySnapshot.forEach(doc => {
+          const data = doc.data();
+          data.date = doc.data().timestamp.toDate().toDateString();
+          data.time = doc.data().timestamp.toDate().toLocaleTimeString();
+          PatientsData.push(data);
+        });
+        setPatient({ Patients: PatientsData });
       });
-      setPatient({ Patients: PatientsData });
-    });
+  }, []);
 
   return (
     <RecordContentWrapper>
-      {Patients
-        ? Patients.Patients.map((patient, i) => (
-            <RecordInnerContent key={i}>
-              <Header>
-                <div>
-                  <FirstName>
-                    <span>FirstName:</span>
-                    <span>{patient.firstName}</span>
-                  </FirstName>
-                  <LastName>
-                    <span>LastName:</span>
-                    <span>{patient.lastName}</span>
-                  </LastName>
-                  <Gender>
-                    <span>Gender:</span>
-                    <span>{patient.gender}</span>
-                  </Gender>
-                </div>
-                <div>
-                  <Email>
-                    <span>Email:</span>
-                    <span>{patient.email}</span>
-                  </Email>
-                  <FirstContact>
-                    <span>Contact:</span>
-                    <span>{patient.firstContact}</span>
-                  </FirstContact>
-                  <DOB>
-                    <span>DOB:</span>
-                    <span>{patient.dob}</span>
-                  </DOB>
-                </div>
-              </Header>
-              <OtherMan>
-                <SecondName>
-                  <span>Who is the medicine for?</span>
-                  <span>{patient.secondName}</span>
-                </SecondName>
-                <SecondDOB>
-                  <span>Contact:</span>
-                  <span>{patient.secondContact}</span>
-                </SecondDOB>
-                <SecondDOB>
-                  <span>DOB:</span>
-                  <span>{patient.seconddob}</span>
-                </SecondDOB>
-              </OtherMan>
-              <Details>
-                <Symptoms>
-                  <span>Symptoms:</span>
-                  <span>{patient.symptoms}</span>
-                </Symptoms>
-                <SymptomsDuration>
-                  <span>Symptoms Duration:</span>
-                  <span>{patient.symptomsduration}</span>
-                </SymptomsDuration>
-                <PreviousMedicaton>
-                  <span>Previous Medication:</span>
-                  <span>{patient.previousMedication}</span>
-                </PreviousMedicaton>
-                <OnGoingMedication>
-                  <span>OnGoing Medication:</span>
-                  <span>{patient.onGoingMedication}</span>
-                </OnGoingMedication>
-                <Pregnant>
-                  <span>Pregnant?</span>
-                  <span>{patient.pregnant}</span>
-                </Pregnant>
-                <Date>
-                  <span>Date:</span>
-                  <span>{patient.date}</span>
-                </Date>
-                <Time>
-                  <span>Time:</span>
-                  <span>{patient.time}</span>
-                </Time>
-              </Details>
-            </RecordInnerContent>
-          ))
-        : ''}
+      {Patients?.Patients.map((patient, i) => (
+        <RecordInnerContent key={i}>
+          <Header>
+            <div>
+              <FirstName>
+                <span>FirstName:</span>
+                <span>{patient.firstName}</span>
+              </FirstName>
+              <LastName>
+                <span>LastName:</span>
+                <span>{patient.lastName}</span>
+              </LastName>
+              <Gender>
+                <span>Gender:</span>
+                <span>{patient.gender}</span>
+              </Gender>
+            </div>
+            <div>
+              <Email>
+                <span>Email:</span>
+                <span>{patient.email}</span>
+              </Email>
+              <FirstContact>
+                <span>Contact:</span>
+                <span>{patient.firstContact}</span>
+              </FirstContact>
+              <DOB>
+                <span>DOB:</span>
+                <span>{patient.dob}</span>
+              </DOB>
+            </div>
+          </Header>
+          <OtherMan>
+            <SecondName>
+              <span>Who is the medicine for?</span>
+              <span>{patient.secondName}</span>
+            </SecondName>
+            <SecondDOB>
+              <span>Contact:</span>
+              <span>{patient.secondContact}</span>
+            </SecondDOB>
+            <SecondDOB>
+              <span>DOB:</span>
+              <span>{patient.seconddob}</span>
+            </SecondDOB>
+          </OtherMan>
+          <Details>
+            <Symptoms>
+              <span>Symptoms:</span>
+              <span>{patient.symptoms}</span>
+            </Symptoms>
+            <SymptomsDuration>
+              <span>Symptoms Duration:</span>
+              <span>{patient.symptomsduration}</span>
+            </SymptomsDuration>
+            <PreviousMedicaton>
+              <span>Previous Medication:</span>
+              <span>{patient.previousMedication}</span>
+            </PreviousMedicaton>
+            <OnGoingMedication>
+              <span>OnGoing Medication:</span>
+              <span>{patient.onGoingMedication}</span>
+            </OnGoingMedication>
+            <Pregnant>
+              <span>Pregnant?</span>
+              <span>{patient.pregnant}</span>
+            </Pregnant>
+            <Date>
+              <span>Date:</span>
+              <span>{patient.date}</span>
+            </Date>
+            <Time>
+              <span>Time:</span>
+              <span>{patient.time}</span>
+            </Time>
+          </Details>
+        </RecordInnerContent>
+      ))}
     </RecordContentWrapper>
   );
 }
